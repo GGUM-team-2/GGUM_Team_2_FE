a3fa3ee521c248b7ba194d03a1e98e0c9b20e80e
ChaeEunCha <cherrycha03@gmail.com>
2024-10-26 11:06:18 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import GlobalStyle from './css/GlobalStyle';
 import Main from './pages/Main';
+import LandingPage from './pages/LandingPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/SignupPage" element={<SignupPage/>} />
             <Route path="/register" element={<Main/>} />
           </Routes>
           {/* <Navbar /> 항상 하단에 고정되는 Navbar */}
