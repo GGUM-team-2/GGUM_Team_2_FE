8115cd05a1727412fba6442a3e4adf6877daa2d6
cwonni <dlfgnsdkd1004@gmail.com>
2024-10-26 17:37:00 +0900

    마이페이지

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import SignupPage from './pages/SignupPage';
 import Navbar from './components/Navbar';
 import Post from './pages/Post';
 import ItemDetail from './pages/ItemDetail';
+import MyPage from './pages/MyPage';
+import MyPost from './pages/MyPost';
 
 function App() {
   return (
@@ -18,9 +20,11 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage/>} />
             <Route path="/signup" element={<SignupPage/>} />
-            <Route path="/detail" element={<ItemDetail />} /> {/* 새로운 라우트 추가 */}
+            <Route path="/detail" element={<ItemDetail />} /> 
             <Route path="/post" element={<Post/>} />
             <Route path="/main" element={<Main/>} />
+            <Route path="/my" element={<MyPage/>} />
+            <Route path="/my-post" element={<MyPost/>} />
           </Routes>
           <Navbar/>
         </div>
